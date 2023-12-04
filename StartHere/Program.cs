827c000b5141e1fc710f3607be3220f8cae75f3d
Rebeca Felix <97916363+Rebeca-Felix@users.noreply.github.com>
2023-12-04 17:31:46 -0300

    Add files via upload

---
+++ StartHere/Program.cs
@@ -0,0 +1,27 @@
+﻿// First you need to know that double / means a observation, it will not interfere in you code
+
+/* If you want to jump line do this / and * 
+like this it will continue your observation
+until you put this -> */
+
+using System;
+
+//Namespace is the name of the folder where your project are
+namespace StartHere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hello World in C# and with Console.WriteLine ("blabla"); it's a output, this is how you will show the user what you want.
+
+            Console.WriteLine("Hello World!");
+
+            //Wanto more things?
+
+            Console.WriteLine("My name is Rebeca Felix, nice to meet ya! \n Now you know how to jump line ;D");
+            
+            // \n you jump line in your outputs 
+        }
+    }
+}
